a678bfa2aa80086d34bf40fecf15531fb89af869
FrDiego17 <diego2006franco@gmail.com>
2025-04-08 19:25:32 -0600

    Error de register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
@@ -16,8 +17,25 @@ class RegisterController extends Controller
         return view('auth.login_regis');
     }
 
-    public function register(registerRequest $request){
-        $user = user::create($request->validated());
-        return redirect ('/login')->with('Success', ' Account created succesfully')->fails();
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|unique:users,username',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ], [
+            'username.unique' => 'Este nombre de usuario ya está en uso.',
+            'email.unique' => 'Este correo electrónico ya está registrado.',
+            'password.min' => 'La contraseña debe tener al menos 8 caracteres.',
+            'password.confirmed' => 'La confirmación de la contraseña no coincide.',
+            
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/register')->withErrors($validator)->withInput();
+        }
+
+        $user = User::create($validator->validated());
+
+        return redirect('/login')->with('success', "Cuenta registrada exitosamente.");
     }
 }
