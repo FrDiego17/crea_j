0b23ce00c398a1b8f4c3455ee3c1101974d8b668
FrDiego17 <diego2006franco@gmail.com>
2025-04-16 23:06:45 -0600

    xd

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,35 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-/**
- * Class User
- *
- * @property $id
- * @property $name
- * @property $email
- * @property $username
- * @property $email_verified_at
- * @property $password
- * @property $remember_token
- * @property $created_at
- * @property $updated_at
- *
- * @package App
- * @mixin \Illuminate\Database\Eloquent\Builder
- */
-class User extends Model
+class User extends Authenticatable
 {
-    
-    protected $perPage = 20;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = ['name', 'email', 'username', 'password'];
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+    ];
 
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
